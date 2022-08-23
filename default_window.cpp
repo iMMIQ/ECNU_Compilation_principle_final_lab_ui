c6ce7c959f16493f1981863b5e21b9c95039cfb1
Yudong An <ayd@immiq.top>
2022-08-23 23:45:20 +0800

    [add] 功能定义

--- default_window.cpp
+++ default_window.cpp
@@ -5,5 +5,24 @@
 DefaultWindow::DefaultWindow() {
   auto builder = Gtk::Builder::create_from_file("ui.glade");
   builder->get_widget("Box", box);
+  builder->get_widget("code_text", code_text);
+  builder->get_widget("IR_text", IR_text);
+  builder->get_widget("asm_text", asm_text);
+  builder->get_widget("result_text", result_text);
+  builder->get_widget("open_file_button", open_file_button);
+  builder->get_widget("clear_button", clear_button);
+  builder->get_widget("run_button", run_button);
+  open_file_button->signal_clicked().connect(
+      sigc::mem_fun(*this, &DefaultWindow::on_open_file_button_clicked));
+  clear_button->signal_clicked().connect(
+      sigc::mem_fun(*this, &DefaultWindow::on_clear_button_clicked));
+  run_button->signal_clicked().connect(
+      sigc::mem_fun(*this, &DefaultWindow::on_run_button_clicked));
   add(*box);
 }
+
+auto DefaultWindow::on_open_file_button_clicked() -> void {}
+
+auto DefaultWindow::on_clear_button_clicked() -> void {}
+
+auto DefaultWindow::on_run_button_clicked() -> void {}
--- default_window.h
+++ default_window.h
@@ -2,16 +2,29 @@
 #define ECNU_COMPILATION_PRINCIPLE_FINAL_LAB_UI_DEFAULT_WINDOW_H
 
 #include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/textview.h>
 #include <gtkmm/window.h>
 
 class DefaultWindow : public Gtk::Window {
 private:
   Gtk::Box *box;
+  Gtk::TextView *code_text;
+  Gtk::TextView *IR_text;
+  Gtk::TextView *asm_text;
+  Gtk::TextView *result_text;
+  Gtk::Button *open_file_button;
+  Gtk::Button *clear_button;
+  Gtk::Button *run_button;
 
 public:
   DefaultWindow();
 
   ~DefaultWindow() override = default;
+
+  auto on_open_file_button_clicked() -> void;
+  auto on_clear_button_clicked() -> void;
+  auto on_run_button_clicked() -> void;
 };
 
 #endif // ECNU_COMPILATION_PRINCIPLE_FINAL_LAB_UI_DEFAULT_WINDOW_H
